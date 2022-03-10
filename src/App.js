1704c61b0facca41bddac339555f6b5517eaa58c
linkmiyu <dbwlsgus77@gmail.com>
2022-03-10 16:48:17 +0900

    const H!

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import mairo from './mairo.png';
 import './App.css';
 
 function App() {
+  const h1Element = {<h1>H! 입니다</h1>};
+  const testData = {
+    
+  };
   return (
     <div className="App">
       <header className="App-header">
+        { h1Element }
         <img src={mairo} className="App-logo" alt="logo" />
         <p>
           응애 나 애기 <code>src/App.js</code> and save to reload.
